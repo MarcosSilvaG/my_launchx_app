ddc26fb40fb573acce87b683863e734e495895d9
Marcos Silva <64155980+MarcosSilvaG@users.noreply.github.com>
2022-04-19 16:54:19 -0500

    Adding objects in index.js file

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const missionCommander = require("./app/missionComander")
 
 const marcos = new missionCommander('Marcos')
+const fer = new missionCommander('Fer')
+const carlo = new missionCommander('Carlo')
+
+console.log(carlo.name)
+console.log(fer.name)
 console.log(marcos.name)
